ea238bbd0db7625e69ba62782d293dae9f90bbb8
Thierry Parent <tparent@absolunet.com>
2019-03-13 14:12:25 -0400

    Renaming source folder to lib, changing terminal version and adding generator command for scaffolding

--- index.js
+++ index.js
@@ -3,4 +3,19 @@
 //--------------------------------------------------------
 'use strict';
 
-module.exports = require('./src/app');
+const app = require('./lib/app');
+const Application = require('./lib');
+const Command = require('./lib/console/Command');
+const GeneratorCommand = require('./lib/console/GeneratorCommand');
+const ServiceProvider = require('./lib/foundation/ServiceProvider');
+const ConsoleServiceProvider = require('./lib/console/BaseConsoleServiceProvider');
+
+// eslint-disable-next-line accessor-pairs
+module.exports = {
+	get app() { return app; },
+	get Application() { return Application; },
+	get Command() { return Command; },
+	get GeneratorCommand() { return GeneratorCommand; },
+	get ServiceProvider() { return ServiceProvider; },
+	get ConsoleServiceProvider() { return ConsoleServiceProvider; }
+};
