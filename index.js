7eb6ce84851f371548141d0af1d6dcf13daeab8e
Thierry Parent <tparent@absolunet.com>
2019-03-14 09:53:24 -0400

    Export classes in bundled property

--- index.js
+++ index.js
@@ -3,19 +3,33 @@
 //--------------------------------------------------------
 'use strict';
 
-const app = require('./lib/app');
+
 const Application = require('./lib');
 const Command = require('./lib/console/Command');
 const GeneratorCommand = require('./lib/console/GeneratorCommand');
 const ServiceProvider = require('./lib/foundation/ServiceProvider');
 const ConsoleServiceProvider = require('./lib/console/BaseConsoleServiceProvider');
 
+
+const hasDriver = require('./lib/foundation/mixins/hasDriver');
+
+
 // eslint-disable-next-line accessor-pairs
 module.exports = {
-	get app() { return app; },
-	get Application() { return Application; },
-	get Command() { return Command; },
-	get GeneratorCommand() { return GeneratorCommand; },
-	get ServiceProvider() { return ServiceProvider; },
-	get ConsoleServiceProvider() { return ConsoleServiceProvider; }
+	get app() { return require('./lib/app'); }, // eslint-disable-line global-require
+	get classes() {
+		// eslint-disable-next-line accessor-pairs
+		return {
+			get Application() { return Application; },
+			get Command() { return Command; },
+			get GeneratorCommand() { return GeneratorCommand; },
+			get ServiceProvider() { return ServiceProvider; },
+			get ConsoleServiceProvider() { return ConsoleServiceProvider; }
+		};
+	},
+	get mixins() {
+		return {
+			hasDriver
+		};
+	}
 };
