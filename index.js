c54c88902c202aefd098303a9c63b27d96338993
Thierry Parent <tparent@absolunet.com>
2019-03-18 13:21:41 -0400

    Create model, mapper and repository system

--- index.js
+++ index.js
@@ -5,10 +5,14 @@
 
 
 const Application = require('./lib');
+const CollectionMapper = require('./lib/foundation/data/CollectionMapper');
 const Command = require('./lib/console/Command');
-const GeneratorCommand = require('./lib/console/GeneratorCommand');
-const ServiceProvider = require('./lib/foundation/ServiceProvider');
 const ConsoleServiceProvider = require('./lib/console/BaseConsoleServiceProvider');
+const GeneratorCommand = require('./lib/console/GeneratorCommand');
+const Mapper = require('./lib/foundation/data/Mapper');
+const Model = require('./lib/foundation/data/Model');
+const Repository = require('./lib/foundation/data/Repository');
+const ServiceProvider = require('./lib/foundation/ServiceProvider');
 
 
 const hasDriver = require('./lib/foundation/mixins/hasDriver');
@@ -21,10 +25,14 @@ module.exports = {
 		// eslint-disable-next-line accessor-pairs
 		return {
 			get Application() { return Application; },
+			get CollectionMapper() { return CollectionMapper; },
 			get Command() { return Command; },
+			get ConsoleServiceProvider() { return ConsoleServiceProvider; },
 			get GeneratorCommand() { return GeneratorCommand; },
-			get ServiceProvider() { return ServiceProvider; },
-			get ConsoleServiceProvider() { return ConsoleServiceProvider; }
+			get Mapper() { return Mapper; },
+			get Model() { return Model; },
+			get Repository() { return Repository; },
+			get ServiceProvider() { return ServiceProvider; }
 		};
 	},
 	get mixins() {
