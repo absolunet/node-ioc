c7833e22b9b28ed48de9adbdd8bd99d1aef8665c
Thierry Parent <tparent@absolunet.com>
2019-04-04 14:43:39 -0400

    Create BaseProxy for common proxy process. Create ForwardProxy and ForwardCalls mixin to allow method call to be forwarded to another party

--- index.js
+++ index.js
@@ -5,9 +5,11 @@
 
 
 const Application = require('./lib');
+const BaseProxy = require('./lib/support/proxy/BaseProxy');
 const CollectionMapper = require('./lib/foundation/data/CollectionMapper');
 const Command = require('./lib/console/Command');
 const ConsoleServiceProvider = require('./lib/console/BaseConsoleServiceProvider');
+const ForwardProxy = require('./lib/support/proxy/ForwardProxy');
 const GeneratorCommand = require('./lib/console/GeneratorCommand');
 const Mapper = require('./lib/foundation/data/Mapper');
 const mixins = require('./lib/support/mixins');
@@ -24,9 +26,11 @@ module.exports = {
 		// eslint-disable-next-line accessor-pairs
 		return {
 			get Application() { return Application; },
+			get BaseProxy() { return BaseProxy; },
 			get CollectionMapper() { return CollectionMapper; },
 			get Command() { return Command; },
 			get ConsoleServiceProvider() { return ConsoleServiceProvider; },
+			get ForwardProxy() { return ForwardProxy; },
 			get GeneratorCommand() { return GeneratorCommand; },
 			get Mapper() { return Mapper; },
 			get Model() { return Model; },
