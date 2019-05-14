f068646b65591e8289b424ebe3d9c6b5dbde4014
Jb Landry <jb@jblandry.info>
2019-05-14 15:27:40 -0400

    Added manager

--- index.js
+++ index.js
@@ -4,19 +4,19 @@
 'use strict';
 
 
-const Application = require('./lib');
-const BaseProxy = require('./lib/support/proxy/BaseProxy');
-const CollectionMapper = require('./lib/foundation/data/CollectionMapper');
-const Command = require('./lib/console/Command');
+const Application            = require('./lib');
+const BaseProxy              = require('./lib/support/proxy/BaseProxy');
+const CollectionMapper       = require('./lib/foundation/data/CollectionMapper');
+const Command                = require('./lib/console/Command');
 const ConsoleServiceProvider = require('./lib/console/BaseConsoleServiceProvider');
-const ForwardProxy = require('./lib/support/proxy/ForwardProxy');
-const GeneratorCommand = require('./lib/console/GeneratorCommand');
-const Mapper = require('./lib/foundation/data/Mapper');
-const mixins = require('./lib/support/mixins');
-const Model = require('./lib/foundation/data/Model');
-const NullDriverProxy = require('./lib/support/drivers/NullDriverProxy');
-const Repository = require('./lib/foundation/data/Repository');
-const ServiceProvider = require('./lib/foundation/ServiceProvider');
+const ForwardProxy           = require('./lib/support/proxy/ForwardProxy');
+const GeneratorCommand       = require('./lib/console/GeneratorCommand');
+const Mapper                 = require('./lib/foundation/data/Mapper');
+const mixins                 = require('./lib/support/mixins');
+const Model                  = require('./lib/foundation/data/Model');
+const NullDriverProxy        = require('./lib/support/drivers/NullDriverProxy');
+const Repository             = require('./lib/foundation/data/Repository');
+const ServiceProvider        = require('./lib/foundation/ServiceProvider');
 
 
 // eslint-disable-next-line accessor-pairs
